0ba562c245c1694fb5e74aef0cadf9fe1baacce6
khoufstudio <rizqy.nurhaqy@gmail.com>
2021-11-16 05:10:03 +0700

    add assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,5 @@
 import LogoKS from "./logo/logo_khoufstudio.svg"
+import BackgroundLP from "./images/background_landingpage.png"
+import BackgroundLP2 from "./images/images background.svg"
 
-export { LogoKS }
+export { LogoKS, BackgroundLP, BackgroundLP2 }
