f24da308225cf2bea1ae29a72efe09cb913e6982
khoufstudio <rizqy.nurhaqy@gmail.com>
2021-11-16 08:05:36 +0700

    update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,60 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Layout from '../components/Layout'
-import { Button, Navbar, Nav } from 'react-bootstrap'
+import { Button, Navbar, Nav, Container } from 'react-bootstrap'
 import Helmet from 'react-helmet'
 import { LogoKS, BackgroundLP2 } from '../assets' 
 
 export default function index() {
   return (
-    <div className="bg-dark" style={{ height: '100vh' }}>
+    <div className="bg-dark" style={{ height: '100vh', backgroundImage: 'linear-gradient(347deg, #201d2b, #858585e3)' }}>
       <Helmet>
         <title>khoufstudio</title>
       </Helmet>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={LogoKS}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            <strong>
+              khoufstudio
+            </strong>
+          </Navbar.Brand>
+           <Nav className="ml-auto">
+            <Nav.Link href="/blog">Blog</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <div className="d-flex text-white mt-5 pt-5"> 
           <img
             alt=""
-            src={LogoKS}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{' '}
-          <strong>
-            khoufstudio
-          </strong>
-        </Navbar.Brand>
-         <Nav className="ml-auto">
-          <Nav.Link href="/blog">Blog</Nav.Link>
-        </Nav>
-      </Navbar>
-      <div className="d-flex text-white mt-5"> 
-        <img
-          alt=""
-          src={BackgroundLP2}
-          className="mx-4 px-4"
-          style={{width: '600px'}}
-        />
-        <div className="text-justify mx-5 px-5">
-          <h2 className="mb-4">
-            Who are we?
-          </h2>
-          <p style={{fontSize: '20px'}}>
-            <strong>khoufstudio</strong> is a software house company that helps people to create a great application. 
-            We can create Web, Mobile and Desktop Apps.
-            Let's gets start making the good product with us! 
-          </p>
-          <Button className="btn btn-light"><strong>Contact Us</strong></Button>
+            src={BackgroundLP2}
+            className="mx-4 px-4"
+            style={{width: '600px'}}
+          />
+          <div className="text-justify mx-5 px-5">
+            <h2 className="mb-4">
+              Who are we?
+            </h2>
+            <p className="text-justify" style={{fontSize: '16px'}}>
+              <strong>khoufstudio</strong> is a software house company that helps people to create a great application. 
+              We can create Web, Mobile and Desktop Apps.
+              Let's gets start making the good product with us! 
+            </p>
+            <Button className="btn btn-light mt-3"><strong>Contact Us</strong></Button>
+          </div>
         </div>
-      </div>
-      
-      <p className="text-center text-white w-100" style={{position: 'absolute', bottom: 0}}>Copyright <strong>2021</strong></p>
+        
+        <div className="position-absolute text-center" style={{ bottom: 0, left: 0, right: 0}}>
+          <p className="text-white">Copyright <strong>2021</strong></p>
+        </div>
+      </Container>
     </div>
   )
 }
